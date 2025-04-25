da3b3534045a5332db91c919da62620ec241a6f3
Jojo <jorgedanielmachado29@gmail.com>
2025-04-25 11:24:16 +0200

    yfeat: adding minio deployment files

--- redact-pdf/controller.py
+++ redact-pdf/controller.py
@@ -1,18 +1,18 @@
 import requests
 
-url = "http://localhost:8000/redact/compute"
+# URLs des microservices
+EXTRACT_URL = "https://nginx.kube.isc.heia-fr.ch/extract-text/compute"
+REDACT_URL = "https://nginx.kube.isc.heia-fr.ch/redact-pdf/compute"
 
 file_path = "../pdfs/in.pdf"
 word_to_redact = "Jorge"
 
 with open(file_path, "rb") as f:
-    files = {"file": f}
-    data = {"word": word_to_redact}
-    response = requests.post(url, files=files, data=data)
-    # print("Status code:", response.status_code)
-    if response.status_code == 200 and response.headers["Content-Type"] == "application/pdf":
-        with open("in_redacted_from_k8s.pdf", "wb") as out_file:
-            out_file.write(response.content)
-        print("PDF censuré téléchargé sous 'in_redacted_from_k8s.pdf'")
-    else:
-        print("Erreur :", response.text)
+    redact_response = requests.post(REDACT_URL, files={"file": f}, data={"word": "Jorge, Machado"})
+
+if redact_response.status_code == 200 and redact_response.headers["Content-Type"] == "application/pdf":
+    with open("in_redacted_from_k8s.pdf", "wb") as out_file:
+        out_file.write(redact_response.content)
+    print("✅ PDF censuré téléchargé sous 'in_redacted_from_k8s.pdf'")
+else:
+    print("❌ Erreur de censure :", redact_response.text)
